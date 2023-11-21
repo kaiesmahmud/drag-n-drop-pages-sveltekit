4fbe0acdf66f963507c13c97debae8a58eb4c04b
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-21 21:46:36 +0600

    meta data added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,11 @@
     import ParentDnd from "../lib/components/ParentDND.svelte";
 
 </script>
+
+<svelte:head>
+    <title>Drag-N-Drop Page Name Editor Sveltekit</title> 
+	<meta name="description" content="Experience effortless page name editing with the Drag-N-Drop Page Name Editor in SvelteKit by Kaies Mahmud Nehal. Seamlessly designed using Tailwind CSS and Svelte-DND-Action, enriched with Iconify icons. Elevate your web development with intuitive drag-and-drop functionality. Unlock user-friendly design and efficiency in one powerful tool" />
+</svelte:head>
 <div class="w-[100%] min-h-screen">
 	<div class=" flex flex-col items-start md:items-center  justify-center gap-10 py-10 px-3">
 		<h3 class="p-2 font-bold text-4xl lg:text-8xl text-transparent capitalize bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
