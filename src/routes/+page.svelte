ccf44761c690581d512ef189435fcd7483c0d71a
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-21 19:12:30 +0600

    home page added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,113 @@
-<h1 class="text-3xl ">
-    Hello world!
-  </h1>
-  
+<script>
+    import DndPageSubPage from '../lib/components/DNDPageSubPage.svelte';
+	let nodes = {
+		node1:{
+				name:'Home',
+				id:'home',
+				items:[ 
+					{id: 'courses'},
+					{id: 'experience'},
+					{id: 'services'}
+				 ], 
+		},
+		courses: {
+			name:'courses', id:"courses"
+		},
+		experience: {
+			name:'experience', id:"experience",items:[]
+		},
+		services: {
+			name:'services',
+			id:"services",
+			 items:[
+				 {id: 'service1'},
+				 {id: 'service2'},
+				 {id: 'service3'},
+				 {id: 'service4'}
+			 ],
+		},
+		service1: {
+			name:'service1', id:"service1"
+		},
+		service2: {
+			name:'service2', id:"service2"
+		},
+		service3: {
+			name:'service3', id:"service3"
+		},
+		service4: {
+			name:'service4', id:"service4"
+		},
+	
+	}
+	
+	console.log("Running")
+	
+	// const addNewNode = ()=>{
+	// 	console.log("addNewNode ")
+	// 	//First Add  to the Root Node Obj
+		
+	// 	// nodes = { ...nodes, node5:{ name:"hello", id: "hello"} }; //node without child
+	// 	nodes = { ...nodes, node5:{ name:"hello", id: "node5", items:[]} }; //node with child
+		
+	// 	//Add to node1 items list to show 
+		
+	// 	nodes['node1'].items.push({id:"node5"}) //EveryNode Have to add in Node1 or Home 
+	// 	console.log(nodes)
+	// }
+	// const addChildNode = ()=> {
+	// 	console.log("AddChildNode ");
+	// 	//Here Node5 will be dynamic
+		
+	// 	nodes.node5.items.push({id: "example",})
+	// 	nodes = { ...nodes, example:{ name:"ChangeName Here", id: "example"} }; //node without child
+	// 	console.log(nodes.node5.items)
+		
+	// }
+
+	let parentId ,newPageName;
+
+	const handleAddNewPage = ()=> {
+		//Create a Node Object 
+		let  pageName = newPageName
+		let  pageURl = newPageName.toLowerCase().replace(/ /g, '-')
+		let pageObj = {
+			name : pageName, id: pageURl , items:[]
+		}
+		console.log(pageObj)
+		newPageName = ""
+		//Add Node to Home items 
+		nodes.node1.items.push({id:pageURl})
+
+		//Add New Page to Main Obj
+		nodes = {...nodes, [pageURl] : pageObj}
+		console.log(nodes)
+		
+	}
+</script>
+<h3>
+	Try drag-n-drop
+</h3>
+<div>
+	<input bind:value={newPageName} />
+	<button on:click={handleAddNewPage}>Add New Page</button>
+</div>
+
+<DndPageSubPage node={nodes.node1} bind:nodes={nodes} parentId={parentId}/>
+
+<br/>
+
+<style>
+	button{
+		border:none;
+		border-radius:5px;
+		margin:5px ;
+		padding:10px;
+	}
+	input{
+		padding:10px;
+	}
+
+</style>
 
 
-
-  <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100);
-    }
-  </style>
