c177a606e87521359a601236abf079a9abfff866
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-22 22:24:56 +0600

    Navbar created & fetched data from state

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+    import NavMainItem from './navMainItem.svelte';
+    export let nodes 
+
+</script>
+<p class="text-lg md:text-2xl text-center font-bold">Show Updated Navbar Here</p>
+<div class="p-5 bg-black/10">
+    {#if nodes?.node1?.items}
+    <div class="flex flex-col md:flex-row md:items-center justify-evenly flex-wrap gap-3">
+            {#each nodes?.node1?.items as mainItem }
+            <NavMainItem mainItem={mainItem} nodes={nodes} />
+            {/each}
+    </div>
+    {/if}
+</div>
