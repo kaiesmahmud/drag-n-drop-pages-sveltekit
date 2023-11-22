ead26a89ba571d34e18a8df341459032482d9db9
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-22 22:24:33 +0600

    state updates functions added

--- src/lib/components/ParentDND.svelte
+++ src/lib/components/ParentDND.svelte
@@ -1,48 +1,6 @@
 <script>
     import DndPageSubPage from '$lib/components/DNDPageSubPage.svelte';
-	let nodes = {
-		node1:{
-				name:'Home',
-				id:'home',
-				items:[ 
-					{id: 'courses'},
-					{id: 'experience'},
-					{id: 'services'}
-				 ], 
-		},
-		courses: {
-			name:'courses', id:"courses"
-		},
-		experience: {
-			name:'experience', id:"experience",items:[]
-		},
-		services: {
-			name:'services',
-			id:"services",
-			 items:[
-				 {id: 'service1'},
-				 {id: 'service2'},
-				 {id: 'service3'},
-				 {id: 'service4'}
-			 ],
-		},
-		service1: {
-			name:'service1', id:"service1"
-		},
-		service2: {
-			name:'service2', id:"service2"
-		},
-		service3: {
-			name:'service3', id:"service3"
-		},
-		service4: {
-			name:'service4', id:"service4"
-		},
-	
-	}
-	
-	console.log("Running")
-	
+	export let MenuListResult,nodes ,handleEditNavbar
 	// const addNewNode = ()=>{
 	// 	console.log("addNewNode ")
 	// 	//First Add  to the Root Node Obj
@@ -89,6 +47,12 @@
 		
 	}
 	let inputStyle="p-3 md:p-5 bg-white/10 border-slate-500 focus:border-none w-[70%] rounded"
+	const handleSaveList = () =>{
+		console.log(nodes)
+		
+		MenuListResult.update(()=> nodes)
+		handleEditNavbar()
+	}
 </script>
 
 
@@ -98,8 +62,10 @@
 </div>
 
 <div class="w-[90%] md:w-[80%] lg:w-[50%] p-1">
-    <DndPageSubPage  node={nodes.node1} bind:nodes={nodes} parentId={parentId}/>
+	<DndPageSubPage  node={nodes.node1} bind:nodes={nodes} parentId={parentId}/>
 </div>
+<button class="bg-green-500/20 p-5 rounded w-[30%] " on:click={handleSaveList}>Save List</button>
+
 	
 	
 
